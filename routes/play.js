80251dea167adbebd088820ccefd9a7ec486b9c6
RadeDimitrijevic <juanjs120@outlook.com>
2024-05-17 19:00:15 +0200

    add ABI, integrate web3

--- routes/play.js
+++ routes/play.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var util = require('../config/util.js');
 var router = express.Router();
+const Web3 = require('web3');
+const LLG_ABI = require('../config/LLGABI.js');
+
+const BSC_RPC_ENDPOINT = process.env.BSC_RPC_ENDPOINT;
+const LLG_CONTRACT_ADDRESS = process.env.LLG_CONTRACT_ADDRESS;
 
 router.get('/', function(req, res) {
     res.render('partials/play', {
@@ -16,5 +21,20 @@ router.post('/', function(req, res) {
     var token = util.randomString(20);
     res.redirect('/game/' + token + '/' + side);
 });
+
+router.get('/balance', async function (req, res) {
+    try {
+        const walletAddress = req.query.wallet;
+        const web3 = new Web3(BSC_RPC_ENDPOINT);
+        const LLGContract = new web3.eth.Contract(LLG_ABI, LLG_CONTRACT_ADDRESS);
+        const balance = await LLGContract.methods.balanceOf(walletAddress).call();
+        console.log("balance", balance);
+        res.json({ balance });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
 
 module.exports = router;
